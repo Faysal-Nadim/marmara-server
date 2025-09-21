854aaada5bbeb7a90c344e1eb6882995f2a1aca3
Faysal-Nadim <faysalnadim6@gmail.com>
2025-09-21 21:22:23 +0600

    Deployment

--- src/index.server.js
+++ src/index.server.js
@@ -27,6 +27,8 @@ app.options("*", cors());
 app.use(express.json());
 app.use("/api/v1", utilsRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on PORT ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on PORT ${process.env.PORT}`);
+// });
+
+module.exports = app;
